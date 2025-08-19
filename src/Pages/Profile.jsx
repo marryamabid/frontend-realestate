aca4b0a97b29de9a25b037b599f5b3640c910c07
Marryam Abid <marryam.abid11@gmail.com>
2025-08-19 18:27:22 +0500

    CHNAGE IN SHOWLISTING IN PROFILE JS

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -298,9 +298,7 @@ export default function Profile() {
       <button onClick={handleShowListings} className="text-green-700 w-full">
         Show Listings
       </button>
-      {showListingError && (
-        <p className="text-red-500 mt-2">{showListingError}</p>
-      )}
+      {showListingError && <p className="text-red-500 mt-2">{error}</p>}
       {showListing && showListing.length > 0 && (
         <div className="flex flex-col gap-4">
           <h1 className="text-center mt-7 text-2xl font-semibold mb-2">
